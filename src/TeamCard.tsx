98e79907a3b3ea69e13e825415d35c4a354be485
Minh Le <levuhoangminh1@gmail.com>
2026-09-20 17:35:26 -0400

    made each card clickable, added selection state in TeamList,  show which team is currently selected

--- src/TeamCard.tsx
+++ src/TeamCard.tsx
@@ -2,11 +2,15 @@ import type { Team } from './types';
 
 type TeamCardProps = {
     team: Team;
+    isSelected: boolean;
+    onSelect: () => void;
 }
 
-export default function TeamCard({ team }: TeamCardProps) {
+export default function TeamCard({ team, isSelected, onSelect }: TeamCardProps) {
     return (
-        <div className="team-card">
+        <div className={isSelected ? "team-card selected" : "team-card"}
+             onClick={onSelect}
+        >
             <h3>{team.teamName}</h3>
             <p>{team.school}</p>
         </div>
--- src/TeamList.tsx
+++ src/TeamList.tsx
@@ -1,13 +1,23 @@
 import type { Team } from './types';
+import { useState } from 'react';
 import TeamCard from './TeamCard';
 
 export default function TeamList( { teams }: { teams: Team[] }) {
+    const [selectedTeamId, setSelectedTeamId] = useState<string | null>(null);
+    const selectedTeam = teams.find(team => team.id === selectedTeamId);
+    
     return (
         <div className="team-list">
+            <p>Selected: {selectedTeam ? selectedTeam.teamName : "none"}</p>
+
             {teams.length === 0 ? (
                 <p>No teams yet</p>
             ) : (
-            teams.map((team) => <TeamCard key={team.id} team={team} />)
+                teams.map((team) => 
+                <TeamCard key={team.id} 
+                team={team} 
+                isSelected={selectedTeamId === team.id} 
+                onSelect={() => setSelectedTeamId(team.id)} />)
             )}
         </div>
     );
